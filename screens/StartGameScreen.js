b88a8c7bd1819044663a137853f7c69e2c0b8458
Fanzejie <fanzejiea@gmail.com>
2025-08-06 19:43:41 +0930

    在RN中使用usestate

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,47 +1,73 @@
-import { View, StyleSheet,TextInput } from "react-native";
+import { useState } from "react";
+import { View, StyleSheet, TextInput, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
-    return (
-        <View style={styles.container}>
-            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
-            <View style={styles.buttonsContainer}>
-                <PrimaryButton>Start Game</PrimaryButton>
-                <PrimaryButton>End Game</PrimaryButton>
-            </View>
-        </View>
-    )
-}
+  const [enteredNumber, setEnteredNumber] = useState("");
+  const numberInputHandler = (inputText) => {
+    setEnteredNumber(inputText);
+  };
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99.",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+      );
+    }
+  }
+  function resetInputHandler() {
+    setEnteredNumber("");
+  }
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={enteredNumber}
+        onChangeText={numberInputHandler}
+      />
+      <View style={styles.buttonsContainer}>
+        <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+        <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+      </View>
+    </View>
+  );
+};
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 16,
-        marginTop: 100,
-        marginHorizontal: 24,
-        backgroundColor: "#270516",
-        borderRadius: 8,
-        elevation: 4,
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 4 },
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    numberInput: {
-        height: 60,
-        width: 50,
-        fontSize: 32,
-        borderBottomColor: "#ddb52f",
-        borderBottomWidth: 2,
-        color: "#ddb52f",
-        fontWeight: "bold",
-        textAlign: "center",
-    },
-    buttonsContainer: {
-        marginTop: 24,
-        flexDirection: "row",
-    },
+  container: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#270516",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  numberInput: {
+    height: 60,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    marginTop: 24,
+    flexDirection: "row",
+  },
 });
