0637f13c3ad2721dd577fb372dea3b78351ca116
Fanzejie <fanzejiea@gmail.com>
2025-08-06 19:34:06 +0930

    渐变与背景图片结合，还挺好看的

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from "./screens/StartGameScreen";
 export default function App() {
   return (
     <LinearGradient colors={['#72063c','#ddb52f']} style={styles.appContainer}>
-      <StartGameScreen />
+      <ImageBackground source={require('./assets/dices.jpg')} resizeMode="cover" style={styles.appContainer} imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -12,6 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    padding: 16,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
